bad31d8546c017c6e712249ee80163c5b2ca623b
Elahianmorel <Elahianm@nycstudents.net>
2025-09-11 11:19:39 -0400

    Add files via upload

---
+++ Activity - Grades/public/grades.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",addlistener)
+
+function addListener()
+{
+	document.getElementById("btnlogin").addEventListener("click",login);
+}
+
+function logininfo()
+{
+	class1 = document.getElemnetById("txtgrade1").value;
+	class2 = document.getElement("txtgrade2").value
+}
