d2045bdee0d4bde7ce8741405c903599787940c8
Elahianmorel <Elahianm@nycstudents.net>
2025-09-11 19:41:51 -0400

    grades.js

--- Activity - Grades/public/grades.js
+++ Activity - Grades/public/grades.js
@@ -1,12 +1,28 @@
-window.addEventListener("load",addlistener)
+window.addEventListener("load",addListener);
 
 function addListener()
 {
-	document.getElementById("btnlogin").addEventListener("click",login);
+	document.getElementById("btngrade").addEventListener("click",gradeinfo);
 }
 
-function logininfo()
+function gradeinfo()
 {
-	class1 = document.getElemnetById("txtgrade1").value;
-	class2 = document.getElement("txtgrade2").value
+	g1 = document.getElementById("txtgrade1").value;
+	g2 = document.getElementById("txtgrade2").value;
+	g3 = document.getElementById("txtgrade3").value;
+	g4 = document.getElementById("txtgrade4").value;
+	g5 = document.getElementById("txtgrade5").value;
+	
+	if(g1 == "" || g2 == "" || g3 == "" || g4 == "" || g5 == "" )
+	{
+		alert("Missing grade")
+	}
+	else
+	{
+		overall = (int(g1) + int(g2) + int(g3) + int(g4) + int(g5) )
+		
+		avg = (int(overall)/int(5))
+		alert(str(avg));
+		document.write("your grade is" + str(avg))
+	}
 }
