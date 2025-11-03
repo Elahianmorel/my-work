200001be55c7f565afeab31e0ab2a99f9cb73222
Elahianmorel <Elahianm@nycstudents.net>
2025-11-03 11:18:38 -0500

    info.js

--- Supermarket/public/info.js
+++ Supermarket/public/info.js
@@ -3,11 +3,12 @@ total = 0
 fruit_amount = 0
 vegetable_amount = 0
 snack_amount = 0
+items = [apple, banana, Strawberry, Mango, Dragon, pineapple]
 
 function addListener()
 {	
 	// Fruits //
-	document.getElementById("btnapple").addEventListener("click", fruit)
+	buttons[i].addEventListener("click", fruit);
 	document.getElementById("btnbanana").addEventListener("click", fruit)
 	document.getElementById("btnStrawberry").addEventListener("click", fruit)
 	document.getElementById("btnMango").addEventListener("click", fruit)
@@ -54,6 +55,7 @@ function fruit()
 
 	total = total + 3
 	fruit_amount = fruit_amount + 1
+	alert (total);
 	
 
 }
@@ -74,13 +76,14 @@ function snacks()
 
 	total = total + 7
 	snack_amount = snack_amount + 1
+	alert (total);
 
 }
 
 
 function show()
 {	
-	
+	alert(total)
 	amount = ["your total is $"+total,"you bought " + fruit_amount + " Fruits", "you bought " + vegetable_amount + " vegetables", "you bought " + snack_amount + " snacks" ];
 	
 	amount.innerHTML = "";
