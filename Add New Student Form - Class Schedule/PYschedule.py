6c76293f851257eaf786bc87a050bf843f290b7b
Elahianmorel <Elahianm@nycstudents.net>
2026-04-01 11:19:59 -0400

    Add files via upload

--- Add New Student Form - Class Schedule/PYschedule.py
+++ Add New Student Form - Class Schedule/PYschedule.py
@@ -4,20 +4,29 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Schedule.html')
+    return render_template('Schedule_input.html')
+
+    
 
 @app.route('/output')
 def CollectInfo():
+
+    pd1 = request.form.get('txtpd1')
+    pd2 = request.form.get('txtpd2')
+    pd3 = request.form.get('txtpd3')
+    pd4 = request.form.get('txtpd4')
+    pd5 = request.form.get('txtpd5')
+    pd6 = request.form.get('txtpd6')
+    pd7 = request.form.get('txtpd7')
+    pd8 = request.form.get('txtpd8')
+
+    
     studentinfo = []
 
     studentinfo = [
-            {"monday":  "US HISTORY HONOR"<br>"Room 390", "Tuesday": "he" },
-            {"monday":  "US HISTORY HONOR"<br>"Room 390", "Tuesday": "he" },
-            {"monday":  "US HISTORY HONOR"<br>"Room 390", "Tuesday": "he" },
-            {"monday":  "US HISTORY HONOR"<br>"Room 390", "Tuesday": "he" },
-            {"monday":  "US HISTORY HONOR"<br>"Room 390", "Tuesday": "he" },
+            {"period":  "1", "monday":  pd1, "Tuesday": pd1 , "Wednesday": pd1 , "Thursday": pd1 , "Friday": pd1},
         ]
-    return render_template('schedule_display.html',studentinfo=studentinfo)
+    return render_template('Schedule_input.html',studentinfo=studentinfo)
 
 if __name__ == '__main__':
     app.run()
