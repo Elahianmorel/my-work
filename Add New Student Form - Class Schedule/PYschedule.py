9df7f428e72445ee83d7652adc1df0b656863cb1
Elahianmorel <Elahianm@nycstudents.net>
2026-04-17 11:14:15 -0400

    Pyschedule.py
    Updated the form handling to use request.args instead of request.form and changed the template for output display.

--- Add New Student Form - Class Schedule/PYschedule.py
+++ Add New Student Form - Class Schedule/PYschedule.py
@@ -1,32 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('Schedule_input.html')
-
-    
+    return render_template("schedule_input.html")
 
 @app.route('/output')
 def CollectInfo():
 
-    pd1 = request.form.get('txtpd1')
-    pd2 = request.form.get('txtpd2')
-    pd3 = request.form.get('txtpd3')
-    pd4 = request.form.get('txtpd4')
-    pd5 = request.form.get('txtpd5')
-    pd6 = request.form.get('txtpd6')
-    pd7 = request.form.get('txtpd7')
-    pd8 = request.form.get('txtpd8')
 
     
-    studentinfo = []
+    pd1 = request.args.get("txtpd1")
+    pd2 = request.args.get("txtpd2")
+    pd3 = request.args.get("txtpd3")
+    pd4 = request.args.get("txtpd4")
+    pd5 = request.args.get("txtpd5")
+    pd6 = request.args.get("txtpd6")
+    pd7 = request.args.get("txtpd7")
+    pd8 = request.args.get("txtpd8")
 
-    studentinfo = [
-            {"period":  "1", "monday":  pd1, "Tuesday": pd1 , "Wednesday": pd1 , "Thursday": pd1 , "Friday": pd1},
-        ]
-    return render_template('Schedule_input.html',studentinfo=studentinfo)
 
-if __name__ == '__main__':
+        return render_template("schedule_display.html",pd1=pd1, pd2=pd2, pd3=pd3, pd4=pd4, pd5=pd5, pd6=pd6 ,pd7=pd7, pd8=pd8)
+
+
+if __name__ == "__main__":
     app.run()
