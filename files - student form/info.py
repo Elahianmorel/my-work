2c4056c591c9b495a9f2dda36a0b409141736ca7
Elahianmorel <Elahianm@nycstudents.net>
2026-04-28 11:16:43 -0400

    Update info.py

--- files - student form/info.py
+++ files - student form/info.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, request, url_for
 import os
  
 app = Flask(__name__)
@@ -25,68 +25,58 @@ def CollectInfo():
     pathhway = request.args.get("txtpathway")
     ssn = request.args.get("txtssn")
     osis = request.args.get("txtosis")
-    sid = request.args.get("txtsid")
- 
-
+    sid = request.args.get("txtid")
 
     if middle == "":
         fullname = firstname + " " + lastname
     else:
         fullname = firstname + " " + middle + ". " + lastname
- 
-
 
     if ssn == "" :
         ssn = "N/A"
     else:
         ssn = ssn
- 
 
-
-    info_filename = "StudentInformation" + str(sid) + ".txt"
-    info_filepath = os.path.join(studentinfo, info_filename)
-    with open(info_filepath, 'w') as f:
+    infofilename = "StudentInformation" + str(sid) + ".txt"
+    filepath = os.path.join(studentinfo, infofilename)
+    with open(filepath, 'w') as f:
         f.write("=== STUDENT INFORMATION ===")
         f.write("Full Name:    " + fullname)
         f.write("School:       " + school)
         f.write("Program Type: " + program)
-        f.write("Pathway:      " + pathhway)
+        f.write("Pathway:  " + pathhway)
         f.write("Birth Date:   " + birth)
-        f.write("OSIS:         " + osis)
+        f.write("OSIS: " + osis)
         f.write("SSN/ITIN:     " + ssn)
-        f.write("ID:   " + str(sid) )
- 
+        f.write("ID:   " + str(sid))
 
-
-
-
-    pd1 = request.args.get("txtpd1")
-    pd2 = request.args.get("txtpd2")
-    pd3 = request.args.get("txtpd3")
-    pd4 = request.args.get("txtpd4")
-    pd5 = request.args.get("txtpd5")
-    pd6 = request.args.get("txtpd6")
-    pd7 = request.args.get("txtpd7")
-    pd8= request.args.get("txtpd8")
+        pd1 = request.args.get("txtpd1")
+        pd2 = request.args.get("txtpd2")
+        pd3 = request.args.get("txtpd3")
+        pd4 = request.args.get("txtpd4")
+        pd5 = request.args.get("txtpd5")
+        pd6 = request.args.get("txtpd6")
+        pd7 = request.args.get("txtpd7")
+        pd8= request.args.get("txtpd8")
 
  
-    sched_filename = "StudentSchedule_" + str(sid) + ".txt"
-    sched_filepath = os.path.join(studentinfo, sched_filename)
-    with open(sched_filepath, 'w') as fcreate:
+    schedname = "StudentSchedule" + str(sid) + ".txt"
+    schedname = os.path.join(studentinfo, schedname)
+    with open(filepath, 'w') as fcreate:
         fcreate.write("=== STUDENT SCHEDULE ===")
-        fcreate.write("Full Name:  " + fullname )
-        fcreate.write("Student ID: " + str(sid) )
+        fcreate.write("Full Name:  " + fullname)
+        fcreate.write("Student ID: " + str(sid))
         fcreate.write("--- Schedule ---")
-        fcreate.write("Period 1:  " + pd1 )
-        fcreate.write("Period 2:  " + pd2 )
-        fcreate.write("Period 3:  " + pd3 )
-        fcreate.write("Period 4:  " + pd4 )
-        fcreate.write("Period 5:  " + pd5 )
-        fcreate.write("Period 6:  " + pd6 )
-        fcreate.write("Period 7:  " + pd7 )
-        fcreate.write("Period 8:  " + pd8 )
+        fcreate.write("Period 1: " + pd1 )
+        fcreate.write("Period 2: " + pd2)
+        fcreate.write("Period 3: " + pd3)
+        fcreate.write("Period 4: " + pd4)
+        fcreate.write("Period 5: " + pd5 )
+        fcreate.write("Period 6: " + pd6)
+        fcreate.write("Period 7: " + pd7)
+        fcreate.write("Period 8: " + pd8)
  
-    return render_template("studentform_out.html", school=school, program=program, firstname=firstname, lastname=lastname, birth=birth, pathhway=pathhway, osis=osis, ssn=ssn, middle=middle, sid=sid, pd1=pd1, pd2=pd2, pd3=pd3, pd4=pd4, pd5=pd5, pd6=pd6, pd7=pd7, pd8=pd8, info_file=info_filename, sched_file=sched_filename)
+    return render_template("studentform_out.html", school=school, program=program, firstname=firstname, lastname=lastname, birth=birth, pathhway=pathhway, osis=osis, ssn=ssn, middle=middle, sid=sid, pd1=pd1, pd2=pd2, pd3=pd3, pd4=pd4, pd5=pd5, pd6=pd6, pd7=pd7, pd8=pd8, infofile=infofilename, schedfile=schedname)
  
 if __name__ == "__main__":
     app.run()
