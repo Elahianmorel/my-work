779893ad9157ca5d4f49711331b7c09ff0013ccf
Elahianmorel <Elahianm@nycstudents.net>
2026-05-15 11:08:50 -0400

    Update important.py

--- ATM Management/important.py
+++ ATM Management/important.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/",methods=['POST'])
+@app.route("/",methods=['GET'])
 def index():
     return render_template("Select.html")
 
-@app.route('/userinfo', methods=['POST'])
+
+@app.route("/userinfo",methods=['GET'])
+def signup():
+    return render_template("signup.html")
+    
+@app.route('/userinfo1', methods=['GET'])
 def login():
 
-        return render_template("Login.html")
+            return render_template("Login.html")
 
-
-@app.route('/userinfo', methods=['GET'])
-def signup():
-        return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run()
- 
